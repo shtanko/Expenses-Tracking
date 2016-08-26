696aa1d3595b53eafac88dd8eb5a3238f6803d78
Maksim Shtanko <maxis.shtanko@gmail.com>
2016-08-26 15:04:26 +0300

    MySQL DB settings were added for current project.

--- qm_test_proj/settings.py
+++ qm_test_proj/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'qm_test_proj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'mysqldb.cnf'),
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
