21b4575086e62a5a27b13ccf956ebee74c67a97c
Maksim Shtanko <maxis.shtanko@gmail.com>
2016-10-04 16:31:35 +0300

    Default format for test requests in DRF settings was added.

--- qm_test_proj/settings.py
+++ qm_test_proj/settings.py
@@ -133,5 +133,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
         # 'rest_framework.authentication.BasicAuthentication',
-    )
+    ),
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
 }
