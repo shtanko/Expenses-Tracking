530fc8f3c383b75c003cf423510db709cacd8f8a
Maksim Shtanko <maxis.shtanko@gmail.com>
2016-10-05 17:09:22 +0300

    Misspellings in Expense class description were corrected.

--- expenses/models.py
+++ expenses/models.py
@@ -7,20 +7,20 @@ from users.models import User
 
 class Expense(models.Model):
     """
-    This class describes main application model, every object witch represents
+    This class describes main application model, every object which represents
     a user expense, that have
 
     name:
         short description of an item, often a type of expense
     descr:
-        optionnal field that contains full description that user want to add
+        optional field that contains full description that user want to add
         to that item
     created:
-        date and time, then expense was commited
+        date and time, when expense was commited
     last_modified:
         date and time of the last Expense.save() running
     value:
-        actually an expense value in USD
+        expense value in USD
     """
     name = models.CharField(max_length=100, blank=False)
     descr = models.TextField(blank=True)
