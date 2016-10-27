bd3e30d0f3e08abcef82055d5624fea192e4f729
Maksim Shtanko <maxis.shtanko@gmail.com>
2016-10-27 16:00:33 +0300

    Unused imports were removed.

--- users/views.py
+++ users/views.py
@@ -1,7 +1,3 @@
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.response import Response
-from rest_framework.reverse import reverse
-from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 
 from users.models import User
